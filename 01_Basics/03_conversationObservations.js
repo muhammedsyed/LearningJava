b632feec4c1a564400ff71cf42f4ad45f64887be
muhammedsyed <146471385+muhammedsyed@users.noreply.github.com>
2024-09-03 09:40:41 +0000

    Datatypes Conversions

---
+++ 01_Basics/03_conversationObservations.js
@@ -0,0 +1,35 @@
+let score = null;
+
+//console.log(typeof score);
+
+//console.log ( typeof (score));
+
+let value_number = Number (score);
+//console.log (typeof (value_number));
+//console.log (value_number)
+
+/*
+Can't convert strings into integer values, will return a NaN (Not a Number) value.
+
+true = 1, false = 0
+*/
+
+//let is_loggedin = 1
+
+//let boolean_isloggedin = Boolean (is_loggedin)
+//console.log(boolean_isloggedin);
+
+//let is_loggedin = ""
+
+//let boolean_isloggedin = Boolean (is_loggedin)
+//console.log(boolean_isloggedin);
+
+// In a boolean, if u use an empty string, it'll return false.
+// If u use a stringwith words, itll return true.
+
+let some_Number = 33
+
+let string_Number = String (some_Number)
+
+//console.log (string_Number);
+console.log (typeof string_Number);
